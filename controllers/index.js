bdb32f8fc7c608d5ddf6aa2a8b24d3cbad5ae944
vutanguofa <68674610+vutanguofa@users.noreply.github.com>
2020-11-11 18:32:16 -0700

    adding braintree routes

--- controllers/index.js
+++ controllers/index.js
@@ -6,12 +6,14 @@ const salesHistoryRoutes = require('./salesHistory-routes.js');
 const newOrderRoutes = require('./newOrder-routes.js');
 const processPaymentsRoutes = require('./processPayments-routes.js');
 const newAdminRoutes = require('./newAdmin-routes.js');
+const checkoutRoutes = require('./checkout-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/salesHistory', salesHistoryRoutes);
 router.use('/newOrder', newOrderRoutes);
 router.use('/processPayments', processPaymentsRoutes);
 router.use('/newAdmin', newAdminRoutes);
+router.use('/checkout', checkoutRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
